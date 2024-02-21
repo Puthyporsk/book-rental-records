123e9eabd845dc31e5293b76467e4a42d98b8ba3
Puthypor Sengkeo <sengkeoputhypor@gmail.com>
2024-02-20 20:38:22 -0800

    base work

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App = () => {
   const [students, setStudent] = useState([]);
   const [books, setBook] = useState([]);
+
+  useEffect(() => {
+    getAllStudents();
+    getAllBooks();
+    getAllRentalRecord();
+  }, []);
   
   const getAllStudents = async () => {
     const response = await fetch(
@@ -133,22 +139,8 @@ const App = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <button onClick={getAllStudents}>Get Student</button>
         <button onClick={createStudent}>Add Student</button>
-        <button onClick={getAllBooks}>Get Book</button>
         <button onClick={createBook}>Add Book</button>
-        <button onClick={getAllRentalRecord}>Get Rental Record</button>
         <button onClick={createRentalRecord}>Add Rental Record</button>
       </header>
     </div>
