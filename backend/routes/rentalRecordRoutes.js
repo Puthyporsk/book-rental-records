de09e8ce901c17e0e897e8d223fdafa2da025036
Puthypor Sengkeo <sengkeoputhypor@gmail.com>
2024-04-24 18:11:17 -0700

    removed backend

--- backend/routes/rentalRecordRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import rentalRecordController from '../controllers/rentalRecordController.js';
-
-router.get('/getAll', rentalRecordController.findRentalRecord);
-router.post('/create', rentalRecordController.createRentalRecord);
-
-export default router;
--- backend/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import studentController from '../controllers/studentController.js';
-
-router.get('/getAll', studentController.findStudent);
-router.post('/create', studentController.createStudent);
-router.put('/edit', studentController.editStudent);
-
-export default router;
